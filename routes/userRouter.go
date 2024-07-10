1885dc67dca6b13ca3557f4d165f57459e3bdeb6
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-07-10 17:30:44 +0530

    middleware

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	controller "github.com/shaurya019/controllers"
+	middleware "github.com/shaurya019/middleware"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine){
